1b1ea845da9f4c089e988daa8c0933d4d010ccd4
Accurics <admin@accurics.com>
2021-02-05 16:47:29 +0530

    Auto Remediation By Accurics

--- aws_instance.tf
+++ aws_instance.tf
@@ -5,8 +5,13 @@
 resource "aws_instance" "instanceWithNoVpc" {
   ami           = "some-id"
   instance_type = "t2.micro"
-  
+
   tags = {
     Name = "HelloWorld"
+  }
+
+  metadata_options {
+    http_endpoint = "disabled"
+    http_tokens   = "required"
   }
 }
