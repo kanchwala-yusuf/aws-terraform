9eefe05b9c70bfc4e4aaea7fee9e23289a628bd3
Accurics <admin@accurics.com>
2021-02-05 16:37:31 +0530

    Auto Remediation By Accurics

--- aws_kms_key.tf
+++ aws_kms_key.tf
@@ -4,14 +4,14 @@
 
 
 resource "aws_kms_key" "kmsKeyDisabled" {
-  description         = "KMS key 2"
-  is_enabled          = false
+  description = "KMS key 2"
+  is_enabled  = true
 }
 
 resource "aws_kms_key" "kmsKeyRotationDisabled" {
   description         = "KMS key 2"
   is_enabled          = true
-  enable_key_rotation = false
+  enable_key_rotation = true
 }
 
 resource "aws_kms_key" "kmsKeyNoDeletionWindow" {
@@ -22,19 +22,6 @@ resource "aws_kms_key" "kmsKeyNoDeletionWindow" {
 
 
 resource "aws_kms_key" "kmsKeyExposedPolicy" {
-  description         = "KMS key 2"
-    policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "kms:*"
-      ],
-      "Principal": { "AWS": "*" },
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+  description = "KMS key 2"
+  policy      = "{\"Statement\":[{\"Action\":[\"kms:##kms_action##\"],\"Principal\":\"##principal##\",\"Resource\":\"*\"}],\"Version\":\"2012-10-17\"}"
 }
