1e443e14e4c3a54dac6b9fd18197634837e088fd
Accurics <admin@accurics.com>
2021-02-05 16:47:28 +0530

    Auto Remediation By Accurics

--- aws_db_instance.tf
+++ aws_db_instance.tf
@@ -3,47 +3,53 @@
 #}
 
 resource "aws_db_instance" "secureInstance" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  name                 = "mydb"
-  backup_retention_period = 90
+  allocated_storage                   = 20
+  storage_type                        = "gp2"
+  engine                              = "mysql"
+  engine_version                      = "5.7"
+  instance_class                      = "db.m5.xlarge"
+  name                                = "mydb"
+  backup_retention_period             = 90
   iam_database_authentication_enabled = true
-  auto_minor_version_upgrade = true
+  auto_minor_version_upgrade          = true
+  username                            = "<some_random_username>"
 }
 
 resource "aws_db_instance" "rdsBackupDisbaled" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  name                 = "mydb"
-  backup_retention_period = 0
+  allocated_storage                   = 20
+  storage_type                        = "gp2"
+  engine                              = "mysql"
+  engine_version                      = "5.7"
+  instance_class                      = "db.m5.xlarge"
+  name                                = "mydb"
+  backup_retention_period             = 30
+  iam_database_authentication_enabled = true
+  username                            = "<some_random_username>"
 }
 
 resource "aws_db_instance" "rdsIamAuthDisabled" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  name                 = "mydb"
-  backup_retention_period = 90
-  iam_database_authentication_enabled = false
-  auto_minor_version_upgrade = false
-  publicly_accessible = true
+  allocated_storage                   = 20
+  storage_type                        = "gp2"
+  engine                              = "mysql"
+  engine_version                      = "5.7"
+  instance_class                      = "db.m5.xlarge"
+  name                                = "mydb"
+  backup_retention_period             = 90
+  iam_database_authentication_enabled = true
+  auto_minor_version_upgrade          = true
+  publicly_accessible                 = false
+  username                            = "<some_random_username>"
 }
 
 resource "aws_db_instance" "rdsOldCA" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  name                 = "mydb"
-  backup_retention_period = 0
-  ca_cert_identifier   =  "rds-ca-2015"
+  allocated_storage                   = 20
+  storage_type                        = "gp2"
+  engine                              = "mysql"
+  engine_version                      = "5.7"
+  instance_class                      = "db.m5.xlarge"
+  name                                = "mydb"
+  backup_retention_period             = 30
+  ca_cert_identifier                  = "rds-ca-2019"
+  iam_database_authentication_enabled = true
+  username                            = "<some_random_username>"
 }
